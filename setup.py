76969d850ab3a468a1ac3c16d3ef96088682a9d9
Christopher Toth <q.alpha@gmail.com>
2011-01-03 23:08:30 -0500

    Wrote basic setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+ name = 'platform_utils',
+ version=0.1,
+ description = 'Cross-platform utilities for accomplishing basic tasks',
+ package_dir = {'platform_utils': 'platform_utils'},
+ packages = find_packages(),
+ classifiers = [
+  'Development Status :: 3 - Alpha',
+  'Intended Audience :: Developers',
+  'Programming Language :: Python',
+  'License :: OSI Approved :: MIT License',
+'Topic :: Software Development :: Libraries'
+],
+)
