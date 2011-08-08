d83e666ac81c8d70714842308d13390f5377cd5b
Christopher Toth <q.alpha@gmail.com>
2011-08-08 10:09:33 -0400

    Better support for x64 architecture by refactoring the libloader

--- platform_utils/libloader.py
+++ platform_utils/libloader.py
@@ -4,7 +4,7 @@ import platform
 import os
 
 TYPES = {
- 'Linux': (ctypes.LibraryLoader(ctypes.CDLL), ctypes.CFUNCTYPE, ['.so', '.so64']),
+ 'Linux': (ctypes.LibraryLoader(ctypes.CDLL), ctypes.CFUNCTYPE, {'x86': '.so', 'x86_64': '.so64'}),
  'Darwin': (ctypes.LibraryLoader(ctypes.CDLL), ctypes.CFUNCTYPE, '.dylib'),
 }
 if platform.system() == 'Windows':
@@ -17,35 +17,23 @@ def load_library(library, lib_path=None):
   lib_path = [lib_path]
  for p in lib_path:
   path = os.path.join(p, library)
-  libs = _find_libs(path)
-  for p in libs:
-   if not os.path.exists(p):
-    libs.remove(p)
-  lib = _do_load(libs)
+  lib = _find_lib(path)
+  lib = _do_load(lib)
   if lib is not None:
    return lib
  raise LibraryLoadError('unable to load %r. Provided library path: %r' % (library, path))
 
-def _find_libs(path):
+def _find_lib(path):
  ext = TYPES[platform.system()][2]
  possible_files = []
  if isinstance(ext, basestring):
-  possible_files.append('%s%s' % (path, ext))
- elif isinstance(ext, collections.Sequence):
-  for i in ext:
-   possible_files.append('%s%s' % (path, i))
- return possible_files
-
-def _do_load(files_to_try):
+  return '%s%s' % (path, ext)
+ elif isinstance(ext, collections.Mapping):
+  return '%s%s' % (path, ext[platform.machine()])
+ 
+def _do_load(file):
  loader = TYPES[platform.system()][0] 
- loaded = None
- for n, f in enumerate(files_to_try):
-  try:
-   loaded = loader.LoadLibrary(f)
-  except OSError:
-   if n >= len(files_to_try) - 1:
-    raise
- return loaded
+ return loader.LoadLibrary(file)
 
 def get_functype():
  return TYPES[platform.system()][1]
