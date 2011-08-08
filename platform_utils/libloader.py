34c704dfc8de617ca5d07a2f3f0567e39c5ce7a6
Q <q@qwitter-client.net>
2011-08-08 12:04:18 -0400

    Allow passing arguments to the LoadLibrary function

--- platform_utils/libloader.py
+++ platform_utils/libloader.py
@@ -27,14 +27,14 @@ if platform.system() == 'Windows':
 
 class LibraryLoadError(Exception): pass
 
-def load_library(library, x86_path='.', x64_path='.'):
+def load_library(library, x86_path='.', x64_path='.', *args, **kwargs):
  library = '%s%s' % (TYPES[platform.system()]['prefix'], library)
  if platform.machine() == 'x86':
   path = os.path.join(x86_path, library)
  elif platform.machine() == 'x86_64':
   path = os.path.join(x64_path, library)
  lib = _find_lib(path)
- lib = _do_load(lib)
+ lib = _do_load(lib, *args, **kwargs)
  if lib is not None:
   return lib
  raise LibraryLoadError('unable to load %r. Provided library path: %r' % (library, path))
@@ -43,9 +43,9 @@ def _find_lib(path):
  ext = TYPES[platform.system()]['extension']
  return '%s%s' % (path, ext)
  
-def _do_load(file):
+def _do_load(file, *args, **kwargs):
  loader = TYPES[platform.system()]['loader'] 
- return loader.LoadLibrary(file)
+ return loader.LoadLibrary(file, *args, **kwargs)
 
 def get_functype():
  return TYPES[platform.system()]['functype']
