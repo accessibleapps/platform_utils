c3e46e87ba696965173b38d3b64421626f96644a
Christopher Toth <q.alpha@gmail.com>
2011-09-22 01:16:35 -0400

    Ethnocentric: Libloader: if the system architecture isn't x86-64, try x66 no matter what it is.

--- platform_utils/libloader.py
+++ platform_utils/libloader.py
@@ -40,13 +40,12 @@ def _do_load(file, *args, **kwargs):
 
 def find_library_path(libname, x86_path='.', x64_path='.'):
  libname = '%s%s' % (TYPES[platform.system()]['prefix'], libname)
- if platform.machine() == 'x86':
-  path = os.path.join(x86_path, libname)
- elif platform.machine() == 'x86_64':
+ if platform.machine() == 'x86_64':
   path = os.path.join(x64_path, libname)
+ else:
+  path = os.path.join(x86_path, libname)
  ext = TYPES[platform.system()]['extension']
  return '%s%s' % (path, ext)
   
-
 def get_functype():
  return TYPES[platform.system()]['functype']
