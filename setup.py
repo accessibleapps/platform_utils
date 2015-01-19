ee55302cbd55dc1fd4294a00d77043b2a137452d
Q <q@q-continuum.net>
2015-01-19 12:08:02 -0700

    Switched spaces to tabs

--- setup.py
+++ setup.py
@@ -6,20 +6,20 @@ __version__ = 0.33
 install_requires = []
 import platform
 if platform.system() == 'Windows':
- install_requires.append('winpaths')
+	install_requires.append('winpaths')
 
 
 setup(
- name = 'platform_utils',
- version = __version__,
- description = """Cross-platform utilities for accomplishing some tasks that the stdlib isn't equipped to provide""",
- install_requires = install_requires,
- packages = find_packages(),
- classifiers = [
-  'Development Status :: 3 - Alpha',
-  'Intended Audience :: Developers',
-  'Programming Language :: Python',
-  'License :: OSI Approved :: MIT License',
-  'Topic :: Software Development :: Libraries'
- ],
+	name = 'platform_utils',
+	version = __version__,
+	description = """Cross-platform utilities for accomplishing some tasks that the stdlib isn't equipped to provide""",
+	install_requires = install_requires,
+	packages = find_packages(),
+	classifiers = [
+		'Development Status :: 3 - Alpha',
+		'Intended Audience :: Developers',
+		'Programming Language :: Python',
+		'License :: OSI Approved :: MIT License',
+		'Topic :: Software Development :: Libraries'
+	],
 )
