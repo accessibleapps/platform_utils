693e23cc7964c3d82ce1570fafed0c24cf99d184
Christopher Toth <q.alpha@gmail.com>
2017-02-13 18:45:19 -0700

    Starting work on a README and actually getting this publishable to PyPi

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
+import io
 
-__version__ = 0.33
-
+__version__ = 0.40
 
 install_requires = []
 import platform
@@ -13,10 +13,12 @@ setup(
 	name = 'platform_utils',
 	version = __version__,
 	description = """Cross-platform utilities for accomplishing some tasks that the stdlib isn't equipped to provide""",
+	long_description = io.open('README.rst', encoding='UTF8').read(),
 	install_requires = install_requires,
 	packages = find_packages(),
+	zip_safe = False,
 	classifiers = [
-		'Development Status :: 3 - Alpha',
+		'Development Status :: 4 - Beta',
 		'Intended Audience :: Developers',
 		'Programming Language :: Python',
 		'License :: OSI Approved :: MIT License',
