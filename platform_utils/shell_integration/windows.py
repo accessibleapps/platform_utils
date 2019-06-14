294cc3d580da2322904e039a607a311c1a0dd14a
Christopher Toth <q.alpha@gmail.com>
2019-06-14 11:10:49 -0600

    Windows Shell integration code cleanup

--- platform_utils/shell_integration/windows.py
+++ platform_utils/shell_integration/windows.py
@@ -1,11 +1,19 @@
-import _winreg
+from __future__ import absolute_import, unicode_literals
 
-SHELL_REGKEY = ur"Directory\shell"
+try:
+    import winreg as winreg
+except ImportError:
+    import winreg
+
+SHELL_REGKEY = r"Directory\shell"
+
 
 def context_menu_integrate(item_key_name, item_display_text, item_command):
-	"""Creates an item that will appear in the standard windows context menu."""
-	app_menu_key = _winreg.OpenKey(_winreg.HKEY_CLASSES_ROOT, SHELL_REGKEY, 0, _winreg.KEY_WRITE)
-	menu_item_key = _winreg.CreateKey(app_menu_key, item_key_name)
-	_winreg.SetValueEx(menu_item_key, None, None, _winreg.REG_SZ, item_display_text)
-	item_command_key = _winreg.CreateKey(menu_item_key, 'command')
-	_winreg.SetValueEx(item_command_key, None, None, _winreg.REG_SZ, item_command)
+    """Creates an item that will appear in the standard windows context menu."""
+    app_menu_key = winreg.OpenKey(
+        winreg.HKEY_CLASSES_ROOT, SHELL_REGKEY, 0, winreg.KEY_WRITE
+    )
+    menu_item_key = winreg.CreateKey(app_menu_key, item_key_name)
+    winreg.SetValueEx(menu_item_key, None, None, winreg.REG_SZ, item_display_text)
+    item_command_key = winreg.CreateKey(menu_item_key, "command")
+    winreg.SetValueEx(item_command_key, None, None, winreg.REG_SZ, item_command)
